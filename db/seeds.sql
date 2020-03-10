cac20a2dbff8e5e91d8f40c344f24bd5ff4ac97a
Nune Tangamyan <38163311+nunetang@users.noreply.github.com>
2020-03-09 19:54:01 -0700

    creation of burgers_db

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES('All-American');
+
+
+INSERT INTO burgers(burger_name)
+VALUES('Bacon Cheeseburger');
+
+
+INSERT INTO burgers(burger_name)
+VALUES('New Burger Wow');
+
+
+INSERT INTO burgers(burger_name)
+VALUES('Southern Burger');
+
+
+INSERT INTO burgers(burger_name)
+VALUES('Veggie Burger');
